7fb31c86f20d12a90abdb3161687c0428df526a0
thieunguyencrystal <thieumao@gmail.com>
2019-01-14 02:16:20 +0800

    Initial Commit

---
+++ PokeAppTests/PokeAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  PokeAppTests.swift
+//  PokeAppTests
+//
+//  Created by MAC OSX on 14/1/19.
+//  Copyright © 2019 Thieu Mao. All rights reserved.
+//
+
+import XCTest
+@testable import PokeApp
+
+class PokeAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
